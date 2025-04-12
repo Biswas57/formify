77389376a1dc66aa82f4f6a1eb52b3363cedef50
Biswas57 <biswas.simkhada@student.unsw.edu.au>
2025-04-13 01:16:09 +1000

    production

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -28,9 +28,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "https://formify-yg3d.onrender.com"
+    "localhost",
+    "127.0.0.1",
+]
 
 
 # Application definition
